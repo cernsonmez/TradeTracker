9b59e5f54a49128df8ae25386ec81bd437c8a572
Ceren Sonmez <ceren.sonmez@ibtech.com.tr>
2023-06-04 10:26:25 +0300

    Initial Commit

---
+++ TradeTracker/TradeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeTrackerApp.swift
+//  TradeTracker
+//
+//  Created by Ceren on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TradeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
